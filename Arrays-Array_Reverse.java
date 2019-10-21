ba9c2278317a2ccbd1298206690f33acb486bc27
SamuelSanthosh <35218854+SamuelSanthosh@users.noreply.github.com>
2019-10-21 22:11:28 +0530

    Create Arrays-Array_Reverse.java

---
+++ Arrays-Array_Reverse.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Array
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int arr[]={12,23,45,23,23,13,34,1,2,3,4,5};
+		int n = arr.length;
+		int k=1;
+		for(int i=n-1;i>=0;i--)
+		{
+		        System.out.println(arr[i]);
+		}
+	}
+}
